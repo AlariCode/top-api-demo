e90a14addfc75e1a97cd072364ac56826783b50c
alaricode <antonlarichev@gmail.com>
2021-02-21 19:39:39 +0300

    Добавление модулей

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-	imports: [],
+	imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
