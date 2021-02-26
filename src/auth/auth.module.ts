e35f3d9eb4a9a25d75b069015fde361dc9204158
alaricode <antonlarichev@gmail.com>
2021-02-26 16:15:17 +0300

    fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthController } from './auth.controller';
 
 @Module({
-  controllers: [AuthController]
+	controllers: [AuthController]
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
-import { ProductController } from './product.controller';
 
 @Module({
-  controllers: [ProductController]
+	controllers: [ProductController]
 })
-export class ProductModule {}
+export class ProductModule { }
