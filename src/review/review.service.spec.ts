5add5c7bab61a903f979ef46319b79f2375f8721
alaricode <antonlarichev@gmail.com>
2021-03-01 18:27:13 +0300

    Unit тест review

--- src/review/review.service.spec.ts
+++ src/review/review.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
+import { getModelToken } from 'nestjs-typegoose';
 import { ReviewService } from './review.service';
 
 describe('ReviewService', () => {
 	let service: ReviewService;
 
+	const exec = { exec: jest.fn() };
+	const reviewRepositoryFactory = () => ({
+		find: () => exec
+	});
+
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [ReviewService],
+			providers: [
+				ReviewService,
+				{ useFactory: reviewRepositoryFactory, provide: getModelToken('ReviewModel') }
+			],
 		}).compile();
 
 		service = module.get<ReviewService>(ReviewService);
@@ -14,5 +24,12 @@ describe('ReviewService', () => {
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+
+	it('findByProductId working', async () => {
+		const id = new Types.ObjectId().toHexString();
+		reviewRepositoryFactory().find().exec.mockReturnValueOnce([{ productId: id }]);
+		const res = await service.findByProductId(id);
+		expect(res[0].productId).toBe(id);
 	});
 });
