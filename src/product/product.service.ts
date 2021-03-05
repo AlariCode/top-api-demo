746c041820b8890926792fc7b0e58fa35f507af6
alaricode <antonlarichev@gmail.com>
2021-03-05 13:14:48 +0300

    Функции

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -52,7 +52,17 @@ export class ProductService {
 			{
 				$addFields: {
 					reviewCount: { $size: '$reviews' },
-					reviewAvg: { $avg: '$reviews.rating' }
+					reviewAvg: { $avg: '$reviews.rating' },
+					reviews: {
+						$function: {
+							body: `function (reviews) {
+								reviews.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+								return reviews;
+							}`,
+							args: ['$reviews'],
+							lang: 'js'
+						}
+					}
 				}
 			}
 		]).exec() as (ProductModel & { review: ReviewModel[], reviewCount: number, reviewAvg: number })[];
