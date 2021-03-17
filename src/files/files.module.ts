08aff3587746422c71861739bf9f450fe3fb6937
alaricode <antonlarichev@gmail.com>
2021-03-17 18:04:16 +0300

    Serve файлов

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { path } from 'app-root-path';
 
 @Module({
-  controllers: [FilesController],
-  providers: [FilesService]
+	imports: [ServeStaticModule.forRoot({
+		rootPath: `${path}/uploads`,
+		serveRoot: '/static'
+	})],
+	controllers: [FilesController],
+	providers: [FilesService]
 })
-export class FilesModule {}
+export class FilesModule { }
