ddc6eb33f3b96d58f4c328cf4060933025789f97
alaricode <antonlarichev@gmail.com>
2021-03-24 19:05:39 +0300

    group и push

--- src/top-page/top-page.service.ts
+++ src/top-page/top-page.service.ts
@@ -21,7 +21,15 @@ export class TopPageService {
 	}
 
 	async findByCategory(firstCategory: TopLevelCategory) {
-		return this.topPageModel.find({ firstCategory }, { alias: 1, secondCategory: 1, title: 1, category: 1 }).exec();
+		return this.topPageModel
+			.aggregate()
+			.match({
+				firstCategory
+			})
+			.group({
+				_id: { secondCategory: '$secondCategory' },
+				pages: { $push: { alias: '$alias', title: '$title' } }
+			}).exec();
 	}
 
 	async findByText(text: string) {
