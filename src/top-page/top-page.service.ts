79a16d30f8bf7900963a397ff5fd89fb5042ef17
alaricode <antonlarichev@gmail.com>
2021-03-26 17:10:24 +0300

    added new fields

--- src/top-page/top-page.service.ts
+++ src/top-page/top-page.service.ts
@@ -28,7 +28,7 @@ export class TopPageService {
 			})
 			.group({
 				_id: { secondCategory: '$secondCategory' },
-				pages: { $push: { alias: '$alias', title: '$title' } }
+				pages: { $push: { alias: '$alias', title: '$title', _id: '$_id', category: '$category' } }
 			}).exec();
 	}
 
