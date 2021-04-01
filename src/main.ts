35c9ac3507c66145520db71dcdaac336c0fe64a3
alaricode <antonlarichev@gmail.com>
2021-04-01 13:23:36 +0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('api');
+	app.enableCors();
 	await app.listen(3000);
 }
 bootstrap();
